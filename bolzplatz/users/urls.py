557e0629c8daddd8578829a371e27cf3e927b4f8
Roger Anton <evonaut@sr1005.de>
2016-04-30 16:26:06 +0200

    Implemented user account creation

--- bolzplatz/users/urls.py
+++ bolzplatz/users/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
+
+from .views import CreateAccount
 
 urlpatterns = [
+    url(r'^$',
+        RedirectView.as_view(
+            pattern_name='auth:login',
+            permanent=False)),
     url(r'^login/$',
         auth_views.login,
         {'template_name': 'users/login.html'},
@@ -14,4 +20,12 @@ urlpatterns = [
          'extra_context':
              {'form': AuthenticationForm}},
         name='logout'),
+    url(r'^create/$',
+        CreateAccount.as_view(),
+        name='create'),
+    url(r'^create/done/$',
+        TemplateView.as_view(
+            template_name=(
+                'users/user_create_done.html')),
+        name='create_done'),
 ]
--- bolzplatz/users/views.py
+++ bolzplatz/users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import View
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.template.response import TemplateResponse
 
-# Create your views here.
+class CreateAccount(View):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('auth:create_done')
+    template_name = 'users/user_create.html'
+
+    @method_decorator(csrf_protect)
+    def get(self, request):
+        return TemplateResponse(
+            request,
+            self.template_name,
+            {'form': self.form_class()})
+
+    @method_decorator(csrf_protect)
+    @method_decorator(sensitive_post_parameters('password1', 'password2'))
+    def post(self, request):
+        bound_form = self.form_class(request.POST)
+        if bound_form.is_valid():
+            if bound_form.save():
+                return redirect(self.success_url)
+            else:
+                errs = (
+                    bound_form.non_field_errors())
+                for err in errs:
+                    error(request, err)
+                return redirect(
+                    'auth:create')
+        return TemplateResponse(
+            request,
+            self.template_name,
+            {'form': bound_form})
